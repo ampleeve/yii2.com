12e6aefed180c42b433a5d02a7c7c9aed7e3f7a6
ampleeve <e@ampleev.com>
2017-05-30 17:26:30 +0300

    sdff

--- basic/web/index.php
+++ basic/web/index.php
@@ -1,5 +1,5 @@
 <?php
-echo "WHAT THE f...???";die();
+echo "WHAT THE f...???";
 
 // comment out the following two lines when deployed to production
 //defined('YII_DEBUG') or define('YII_DEBUG', true);
