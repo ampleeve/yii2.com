aaaa414a1329761e882a40c2aa560d08437c9dde
ampleeve <e@ampleev.com>
2017-05-30 16:38:54 +0300

    sdf

--- basic/config/db.php
+++ basic/config/db.php
@@ -3,7 +3,7 @@
 return [
     'class' => 'yii\db\Connection',
     'dsn' => 'mysql:host=127.0.0.1;dbname=shop1',
-    'username' => 'root',
-    'password' => '',
+    'username' => 'testuser',
+    'password' => '123',
     'charset' => 'utf8',
 ];
