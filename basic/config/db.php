fc5553166d4ecf9f89da87cd564178b4a6eee962
ampleeve <e@ampleev.com>
2017-05-30 18:01:49 +0300

    config db fix

--- basic/config/db.php
+++ basic/config/db.php
@@ -3,7 +3,7 @@
 return [
     'class' => 'yii\db\Connection',
     'dsn' => 'mysql:host=127.0.0.1;dbname=shop1',
-    'username' => 'root',
-    'password' => '',
+    'username' => 'redmine',
+    'password' => 'hLAqd47Phe5M',
     'charset' => 'utf8',
 ];
