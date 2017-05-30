b168ca29c7d9036359d722637691aa78f7d13915
ampleeve <e@ampleev.com>
2017-05-30 17:32:08 +0300

    sdf

--- basic/web/index.php
+++ basic/web/index.php
@@ -3,8 +3,8 @@ echo "WHAT THE ???";
 
 // comment out the following two lines when deployed to production
 defined('YII_DEBUG') or define('YII_DEBUG', true);
-echo "before1";
 defined('YII_ENV') or define('YII_ENV', 'dev');
+echo "before2";
 
 require(__DIR__ . '/../vendor/autoload.php');
 require(__DIR__ . '/../vendor/yiisoft/yii2/Yii.php');
