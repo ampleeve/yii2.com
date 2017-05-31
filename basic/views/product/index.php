fe59ab63e3f53f97e21bc8e38e3e06e0fc489764
ampleeve <e@ampleev.com>
2017-05-31 18:15:15 +0300

    dsf

--- basic/views/product/index.php
+++ basic/views/product/index.php
@@ -17,7 +17,7 @@ $this->params['breadcrumbs'][] = $this->title;
     <h1><?= Html::encode($this->title) ?></h1>
     <?php // echo $this->render('_search', ['model' => $searchModel]); ?>
 
-    <?
+    <?php
 
         $dataProvider = new \yii\data\ActiveDataProvider([
 
