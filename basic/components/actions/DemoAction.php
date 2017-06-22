314f0696f703d92dfe5d44f9ad4b405f18256bf4
ampleeve <e@ampleev.com>
2017-06-22 12:27:39 +0300

    again

--- basic/components/actions/DemoAction.php
+++ basic/components/actions/DemoAction.php
@@ -1,12 +1,17 @@
 <?php
-
+namespace app\components\actions;
 /**
  * Created by PhpStorm.
  * User: evgenijampleev
  * Date: 22.06.17
  * Time: 12:12
  */
-class DemoAction
-{
+class DemoAction extends \yii\base\Action{
+
+    public $message;
+
+    public function run(){
+        echo $this->message;
+    }
 
 }
